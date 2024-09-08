345ab4582bdf46a20306fecd4dc930585bffea7f
xyuan99 <xyuan99@gatech.edu>
2024-09-08 01:08:53 -0400

    modify filter condition, add annotation

--- find_homologs.sh
+++ find_homologs.sh
@@ -3,8 +3,10 @@ subjectFile=$2
 outputFile=$3
 
 # run blast
-tblastn -query queryFile -subject subjectFile -outfmt "6 std qlen" -out raw_match
-awk '$3==100.000 && $4==$13' raw_match > $3
+tblastn -query $queryFile -subject $subjectFile -outfmt "6 std qlen" -out raw_match
+
+# filter raw output
+awk '$3 > 30.000 && $4 / $13 > 0.9' raw_match > $outputFile
 rm raw_match
 filename=$(echo $3|cut -d. -f1)
 wc -l $3 |awk '{print "There are "$1" perfect matches in file "$2}' > $filename.stdout
