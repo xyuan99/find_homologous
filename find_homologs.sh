431b805bf445461268589daccf5b082996aca945
xyuan99 <xyuan99@gatech.edu>
2024-09-08 01:18:11 -0400

    add print message to user

--- find_homologs.sh
+++ find_homologs.sh
@@ -8,5 +8,6 @@ tblastn -query $queryFile -subject $subjectFile -outfmt "6 std qlen" -out raw_ma
 # filter raw output
 awk '$3 > 30.000 && $4 / $13 > 0.9' raw_match > $outputFile
 rm raw_match
-filename=$(echo $3|cut -d. -f1)
-wc -l $3 |awk '{print "There are "$1" perfect matches in file "$2}' > $filename.stdout
+
+# print matched information
+wc -l $outputFile |awk '{print "There are "$1" qualified matches in file "$2}' 
