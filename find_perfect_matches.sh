bbccdac8bcb662d427e8f349b0d03efad151a21b
xyuan99 <xyuan99@gatech.edu>
2024-09-07 22:17:22 -0400

    Add find_perfect_matches.sh script

---
+++ find_perfect_matches.sh
@@ -0,0 +1,5 @@
+blastn -query $1 -subject $2 -task blastn-short -outfmt "6 std qlen" -out raw_match
+awk '$3==100.000 && $4==$13' raw_match > $3
+rm raw_match
+filename=$(echo $3|cut -d. -f1)
+wc -l $3 |awk '{print "There are "$1" perfect matches in file "$2}' > $filename.stdout
